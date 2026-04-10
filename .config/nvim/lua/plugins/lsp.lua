fd71ab290a898a8c2406b270c44e8739bbdfbae4
Christian Schneider <strcat1974@gmail.com>
2026-04-10 20:37:41 +0200

    client.supports_method is obsolete

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,26 +13,26 @@ return {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.document_highlight,
-					})
-					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-						buffer = event.buf,
-						group = highlight_augroup,
-						callback = vim.lsp.buf.clear_references,
-					})
-					vim.api.nvim_create_autocmd("LspDetach", {
-						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-						callback = function(event2)
-							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-						end,
-					})
-				end
+				-- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+				-- 	local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+				-- 	vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				-- 		buffer = event.buf,
+				-- 		group = highlight_augroup,
+				-- 		callback = vim.lsp.buf.document_highlight,
+				-- 	})
+				-- 	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				-- 		buffer = event.buf,
+				-- 		group = highlight_augroup,
+				-- 		callback = vim.lsp.buf.clear_references,
+				-- 	})
+				-- 	vim.api.nvim_create_autocmd("LspDetach", {
+				-- 		group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+				-- 		callback = function(event2)
+				-- 			vim.lsp.buf.clear_references()
+				-- 			vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+				-- 		end,
+				-- 	})
+				-- end
 			end,
 		})
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
